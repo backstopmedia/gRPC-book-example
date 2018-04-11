397905cbe334ad6193ac06ab4a0babdd2b4b225d
David Muto (pseudomuto) <david.muto@gmail.com>
2018-04-10 20:15:28 -0400

    Implement Starships API

--- server/api/starships.go
+++ server/api/starships.go
@@ -6,10 +6,20 @@ import (
 	pb "github.com/backstopmedia/gRPC-book-example/server/proto"
 )
 
-func (s *Server) GetStarship(context.Context, *pb.GetStarshipRequest) (*pb.GetStarshipResponse, error) {
-	return nil, s.notImplementedError()
+func (s *Server) GetStarship(ctx context.Context, r *pb.GetStarshipRequest) (*pb.GetStarshipResponse, error) {
+	starship, err := s.db.GetStarshipByID(ctx, r.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetStarshipResponse{Starship: starship}, nil
 }
 
-func (s *Server) ListStarships(context.Context, *pb.ListStarshipsRequest) (*pb.ListStarshipsResponse, error) {
-	return nil, s.notImplementedError()
+func (s *Server) ListStarships(ctx context.Context, r *pb.ListStarshipsRequest) (*pb.ListStarshipsResponse, error) {
+	starships, err := s.db.GetStarships(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.ListStarshipsResponse{Starships: starships}, nil
 }
