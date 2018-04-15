ab478eaecb240d432628f74ed968f2511f29b8d7
David Konsumer <konsumer@jetboystudio.com>
2018-04-14 17:28:30 -0700

    more sfapi renames

--- tools/demo_resolvers.js
+++ tools/demo_resolvers.js
@@ -6,84 +6,86 @@ const _ = require('lodash')
  */
 
 module.exports = {
-  swapi: {
-    Starwars: {
-      GetFilm: ({ request: { id } }, cb) => {
-        const results = _.find(data.films, {id})
-        if (!results) {
-          return cb({message: 'Record not found', code: 5})
+  sfapi: {
+    v1: {
+      Starfriends: {
+        GetFilm: ({ request: { id } }, cb) => {
+          const results = _.find(data.films, {id})
+          if (!results) {
+            return cb({message: 'Record not found', code: 5})
+          }
+          cb(null, results)
+        },
+
+        ListFilms: (ctx, cb) => {
+          const results = data.films
+          cb(null, {results})
+        },
+
+        GetStarship: ({ request: { id } }, cb) => {
+          const results = _.find(data.starships, {id})
+          if (!results) {
+            return cb({message: 'Record not found', code: 5})
+          }
+          cb(null, results)
+        },
+
+        ListStarships: (ctx, cb) => {
+          const results = data.starships
+          cb(null, {results})
+        },
+
+        GetVehicle: ({ request: { id } }, cb) => {
+          const results = _.find(data.vehicles, {id})
+          if (!results) {
+            return cb({message: 'Record not found', code: 5})
+          }
+          cb(null, results)
+        },
+
+        ListVehicles: (ctx, cb) => {
+          const results = data.vehicles
+          cb(null, {results})
+        },
+
+        GetSpecies: ({ request: { id } }, cb) => {
+          const results = _.find(data.species, {id})
+          if (!results) {
+            return cb({message: 'Record not found', code: 5})
+          }
+          cb(null, results)
+        },
+
+        ListSpecies: (ctx, cb) => {
+          const results = data.species
+          cb(null, {results})
+        },
+
+        GetPlanet: ({ request: { id } }, cb) => {
+          const results = _.find(data.planets, {id})
+          if (!results) {
+            return cb({message: 'Record not found', code: 5})
+          }
+          cb(null, results)
+        },
+
+        ListPlanets: (ctx, cb) => {
+          const results = data.planets
+          cb(null, {results})
+        },
+
+        GetPerson: ({ request: { id } }, cb) => {
+          const results = _.find(data.people, {id})
+          if (!results) {
+            return cb({message: 'Record not found', code: 5})
+          }
+          cb(null, results)
+        },
+
+        ListPeople: (ctx, cb) => {
+          const results = data.people
+          cb(null, {results})
         }
-        cb(null, results)
-      },
-
-      ListFilms: (ctx, cb) => {
-        const results = data.films
-        cb(null, {results})
-      },
-
-      GetStarship: ({ request: { id } }, cb) => {
-        const results = _.find(data.starships, {id})
-        if (!results) {
-          return cb({message: 'Record not found', code: 5})
-        }
-        cb(null, results)
-      },
-
-      ListStarships: (ctx, cb) => {
-        const results = data.starships
-        cb(null, {results})
-      },
-
-      GetVehicle: ({ request: { id } }, cb) => {
-        const results = _.find(data.vehicles, {id})
-        if (!results) {
-          return cb({message: 'Record not found', code: 5})
-        }
-        cb(null, results)
-      },
-
-      ListVehicles: (ctx, cb) => {
-        const results = data.vehicles
-        cb(null, {results})
-      },
-
-      GetSpecies: ({ request: { id } }, cb) => {
-        const results = _.find(data.species, {id})
-        if (!results) {
-          return cb({message: 'Record not found', code: 5})
-        }
-        cb(null, results)
-      },
-
-      ListSpecies: (ctx, cb) => {
-        const results = data.species
-        cb(null, {results})
-      },
-
-      GetPlanet: ({ request: { id } }, cb) => {
-        const results = _.find(data.planets, {id})
-        if (!results) {
-          return cb({message: 'Record not found', code: 5})
-        }
-        cb(null, results)
-      },
-
-      ListPlanets: (ctx, cb) => {
-        const results = data.planets
-        cb(null, {results})
-      },
-
-      GetPerson: ({ request: { id } }, cb) => {
-        const results = _.find(data.people, {id})
-        if (!results) {
-          return cb({message: 'Record not found', code: 5})
-        }
-        cb(null, results)
-      },
-
-      ListPeople: (ctx, cb) => {
-        const results = data.people
-        cb(null, {results})
       }
     }
   }
