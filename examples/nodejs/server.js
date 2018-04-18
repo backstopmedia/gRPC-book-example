f4e099f6279af20832318dcefdb94c123fe6ac22
David Konsumer <konsumer@jetboystudio.com>
2018-04-17 20:56:29 -0700

    further simplified list callbacks

--- examples/nodejs/server.js
+++ examples/nodejs/server.js
@@ -11,10 +11,7 @@ const getHandler = (id, recordName, dataName) => {
   }
 }
 
-const listHandler = (recordName, dataName) => {
-  const records = ({[recordName]: data[dataName]})
-  return records
-}
+const listHandler = (recordName) => ({[recordName]: data[recordName]})
 
 // this could all be generated from a few nouns
 // but here it is all spelled-out for illustration
@@ -23,17 +20,17 @@ module.exports = {
     v1: {
       Starwars: {
         GetFilm: ({request: {id}}) => getHandler(id, 'film', 'films'),
-        ListFilms: () => listHandler('films', 'films'),
+        ListFilms: () => listHandler('films'),
         GetVehicle: ({request: {id}}) => getHandler(id, 'vehicle', 'vehicles'),
-        ListVehicles: () => listHandler('vehicles', 'vehicles'),
+        ListVehicles: () => listHandler('vehicles'),
         GetStarship: ({request: {id}}) => getHandler(id, 'starship', 'starships'),
-        ListStarships: () => listHandler('starships', 'starships'),
+        ListStarships: () => listHandler('starships'),
         GetSpecies: ({request: {id}}) => getHandler(id, 'species', 'species'),
-        ListSpecies: () => listHandler('species', 'species'),
+        ListSpecies: () => listHandler('species'),
         GetPlanet: ({request: {id}}) => getHandler(id, 'planet', 'planets'),
-        ListPlanets: () => listHandler('planets', 'planets'),
+        ListPlanets: () => listHandler('planets'),
         GetPerson: ({request: {id}}) => getHandler(id, 'person', 'people'),
-        ListPeople: () => listHandler('people', 'people'),
+        ListPeople: () => listHandler('people'),
         ListStarshipActions: () => {} // NO-OP
       }
     }
