8c5c79e37bd1aef0854163e8345a5583e2fd6efd
David Konsumer <konsumer@jetboystudio.com>
2018-04-22 12:59:12 -0700

    ValidateSpecies in JS and comments for proto

--- examples/nodejs/server.js
+++ examples/nodejs/server.js
@@ -19,6 +19,7 @@ setInterval(() => {
   starshipTracker.emit('StarshipAction', {action, starship})
 }, 1000)
 
+// generic handler for Get* RPCs
 const getHandler = (id, recordName, dataName) => {
   const record = data[dataName].find(r => r.id === id)
   if (record) {
@@ -30,24 +31,46 @@ const getHandler = (id, recordName, dataName) => {
   }
 }
 
+// generic handler for most List* RPCs
 const listHandler = (recordName) => ({[recordName]: data[recordName]})
 
 module.exports = {
   sfapi: {
     v1: {
       Starfriends: {
-        GetFilm: ({request: {id}}) => getHandler(id, 'film', 'films'),
+        GetFilm: ({ request: { id } }) => getHandler(id, 'film', 'films'),
+
         ListFilms: () => listHandler('films'),
-        GetVehicle: ({request: {id}}) => getHandler(id, 'vehicle', 'vehicles'),
+
+        GetVehicle: ({ request: { id } }) => getHandler(id, 'vehicle', 'vehicles'),
+
         ListVehicles: () => listHandler('vehicles'),
-        GetStarship: ({request: {id}}) => getHandler(id, 'starship', 'starships'),
+
+        GetStarship: ({ request: { id } }) => getHandler(id, 'starship', 'starships'),
+
         ListStarships: () => listHandler('starships'),
-        GetSpecies: ({request: {id}}) => getHandler(id, 'species', 'species'),
+
+        GetSpecies: ({ request: { id } }) => getHandler(id, 'species', 'species'),
+
         ListSpecies: () => listHandler('species'),
-        GetPlanet: ({request: {id}}) => getHandler(id, 'planet', 'planets'),
+
+        GetPlanet: ({ request: { id } }) => getHandler(id, 'planet', 'planets'),
+
         ListPlanets: () => listHandler('planets'),
-        GetPerson: ({request: {id}}) => getHandler(id, 'person', 'people'),
+
+        GetPerson: ({ request: { id } }) => getHandler(id, 'person', 'people'),
+
         ListPeople: () => listHandler('people'),
+
+        ValidateSpecies: ({ request: { name } }) => {
+          if (name.length < 2) {
+            const error = new Error('The name provided is not long enough')
+            error.code = 3
+            return Promise.reject(error)
+          }
+          return {}
+        },
+
         ListStarshipActions: (call) => {
           const handler = ev => call.write(ev)
           starshipTracker.on('StarshipAction', handler)
