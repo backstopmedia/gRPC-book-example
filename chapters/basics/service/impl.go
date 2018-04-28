2701f8da2d03eae8cce2f85dbacf71b2e6f63315
Josh Humphries <jh@fullstory.com>
2018-04-28 10:53:31 -0400

    finalize examples for basics

--- chapters/basics/service/impl.go
+++ chapters/basics/service/impl.go
@@ -1,12 +1,15 @@
 package service
 
 import (
+  "log"
   "time"
 
   "github.com/golang/protobuf/ptypes"
   "github.com/golang/protobuf/ptypes/timestamp"
   "golang.org/x/net/context"
+  "google.golang.org/grpc"
   "google.golang.org/grpc/codes"
+  "google.golang.org/grpc/metadata"
   "google.golang.org/grpc/status"
 
   "../proto"
@@ -53,6 +56,27 @@ type StarfriendsImpl struct {
 func (s *StarfriendsImpl) GetFilm(ctx context.Context,
     req *proto.GetFilmRequest) (*proto.GetFilmResponse, error) {
 
+  respHdrs := metadata.New(map[string]string{
+    "Who":     "starfriends-server",
+    "Version": "v1",
+  })
+  grpc.SendHeader(ctx, respHdrs)
+
+  start := time.Now()
+  defer func() {
+    respTrlrs := metadata.Pairs("duration", time.Since(start).String())
+    grpc.SetTrailer(ctx, respTrlrs)
+  }()
+
+  if reqHdrs, ok := metadata.FromIncomingContext(ctx); ok {
+    log.Printf("Request headers: %v", reqHdrs)
+    // Must use all-lower-case keys to query metadata
+    if who, ok := reqHdrs["who"]; ok {
+      // who is a slice of strings; just use the first
+      log.Printf("Received request from %s", who[0])
+    }
+  }
+
   var film *proto.Film
   for _, f := range films {
     if f.Id == req.Id {
@@ -63,7 +87,9 @@ func (s *StarfriendsImpl) GetFilm(ctx context.Context,
   if film == nil {
     return nil, status.Errorf(codes.NotFound, "no film with id %q", req.Id)
   }
-  return &proto.GetFilmResponse{Film: film}, nil
+  resp := &proto.GetFilmResponse{Film: film}
+
+  return resp, nil
 }
 
 // ListFilms returns a list of all known films.
--- chapters/basics/test_client.rb
+++ chapters/basics/test_client.rb
@@ -5,12 +5,27 @@ require_relative './proto/sfapi_services_pb'
 stub = Starfriends::Stub.new("localhost:8080", :this_channel_is_insecure)
 
 # Now we can use the stub to make RPCs
-request = GetFilmRequest.new(id: '7')
+request = GetFilmRequest.new(id: '4')
 begin
-  response = stub.get_film(request)
+  response = stub.get_film(request,
+      metadata: {'who' => 'starfriends-ruby-client', 'version' => ['v1','v2']})
   puts response.inspect
 rescue GRPC::BadStatus => e
   # RPC exceptions have a numeric 'code' field (the gRPC error code)
   # and a string 'details' (the message supplied by the server)
   puts "failed: #{e.details} (code = #{e.code})"
 end
+
+# We'll make another request and also print the response metadata
+request = GetFilmRequest.new(id: '7')
+op = stub.get_film(request, return_op: true)
+begin
+  op.execute
+  puts response.inspect
+rescue GRPC::BadStatus => e
+  # RPC exceptions have a numeric 'code' field (the gRPC error code)
+  # and a string 'details' (the message supplied by the server)
+  puts "failed: #{e.details} (code = #{e.code})"
+end
+puts "server provided headers: #{op.metadata}"
+puts "server provided trailers: #{op.trailing_metadata}"
